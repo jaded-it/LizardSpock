09a177dd2b3aa785b1b6e454abf599cddede59ea
Jaded <jaded@jaded.it>
2022-07-31 19:52:06 +1000

    Added a round system wherein the first player to wins 3 rounds is the victor

--- scripts/script.js
+++ scripts/script.js
@@ -41,15 +41,51 @@ function getRoundResult(player_action, computer_action_index) {
     let victory_verb;
     switch (player_action_index) {
         case computer_action_index:
-            return `You drew because you both chose ${player_action}`;
+            return [0, `You drew because you both chose ${player_action}`];
         case player_winning_action:
         case player_alternate_winning_action:
             victory_verb = getVictoryVerb(player_action_index, computer_action_index);
-            return `You won because ${player_action} ${victory_verb} ${ALL_ACTIONS[computer_action_index]}`;
+            return [1, `You won because ${player_action} ${victory_verb} ${ALL_ACTIONS[computer_action_index]}`];
         default:
             victory_verb = getVictoryVerb(computer_action_index, player_action_index);
-            return `You lost because ${ALL_ACTIONS[computer_action_index]} ${victory_verb} ${player_action}`;
+            return [2, `You lost because ${ALL_ACTIONS[computer_action_index]} ${victory_verb} ${player_action}`];
     }
 }
 
-console.log(getRoundResult('lizard', getComputerActionIndex()));
+function game() {
+    let player_win_count = 0;
+    let computer_win_count = 0;
+    let round_number = 1;
+    // First to 3 game
+    for (;; round_number++) {
+        let round_result = getRoundResult('lizard', getComputerActionIndex());
+        console.log(round_result[1]); // Output round result message
+
+        // Get who one and increment respectively
+        switch (round_result[0]) {
+            case 1: // Player victory
+                player_win_count++
+                break;
+            case 2: // Computer victory
+                computer_win_count++
+                break;
+            default: // Draw
+                break;
+        }
+
+        // If either player has one, display appropriate message, then break the game loop
+        if (player_win_count >= 3) {
+            console.log("\nVictory! You won the LizardSpock best of 5!");
+            break;
+        } else if (computer_win_count >= 3) {
+            console.log("\nDefeat! You lost the LizardSpock best of 5 :C");
+            break;
+        }
+    }
+
+    // Output game stats
+    const draw_count = round_number - player_win_count - computer_win_count;
+    console.log(`Rounds played: ${round_number}\nWins: ${player_win_count}\nLosses: ${computer_win_count}\nDraws: ${draw_count}`);
+}
+
+game();
